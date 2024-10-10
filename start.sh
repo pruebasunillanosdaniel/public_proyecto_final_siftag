3bdc8cbc1e24d2d33db782162af67ad7bbb58433
pruebasunillanosdaniel <servidor@DESKTOP-2F2NP0K>
2024-10-10 09:05:09 -0500

    10 oct 2024

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "endpoint="minio_server:9000" # servicio minio 
+echo -e "endpoint="minio-server:9000" # servicio minio 
 correo_usuario="danielsalga50@hotmail.es"    
 password_usuario="aid573move841"    
-server_host=http://golang_servicios:9091/ #servicio golang ip  
+server_host=http://10.1.5.145/golang_servicio/ #servicio golang ip  
 accessKeyID="admin" 
 secretAccessKey="password"  
 smtp_server=smtp-mail.outlook.com  
@@ -11,14 +11,18 @@ POSTGRES_USER=tesis
 POSTGRES_DB=diagrama  
 POSTGRES_PASSWORD=unillanos   
 server_java=http://pdf:8090/  
-POSTGRES_HOST_HOST=base_datos   " > .env  
+POSTGRES_HOST_HOST=base_datos 
+REACT_APP_HOST_IP_SERVER=http://10.1.5.145/golang_servicio
+REACT_APP_HOST_IP_ADDRESS=http://10.1.5.145/react
+  " > .env  
 
 export Private_key=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
 export username=pruebasunillanosdaniel
 #echo -e "machine github.com login ${username} password ${Private_key}" >>  $HOME/.netrc 
 #export GOPRIVATE=github.com/pruebasunillanosdaniel/*
-cd bin/
-
+mkdir -p bin/
+cd bin
+echo -e  "1 $PWD"
 if [ ! -d "formatos_proyecto_final" ] ; then
         git clone   https://${username}:${Private_key}@github.com/pruebasunillanosdaniel/formatos_proyecto_final
 else
@@ -26,6 +30,8 @@ else
         git pull https://${username}:${Private_key}@github.com/pruebasunillanosdaniel/formatos_proyecto_final main
         cd ..
 fi
+echo -e  "2 $PWD"
+
 
 
 if [ ! -d "Proyecto_final_tesis" ]; then 
@@ -36,6 +42,7 @@ else
         cd ..
 fi
 
+echo -e  "3 $PWD"
 
 if [ ! -d "react_interface_proyectotesis" ];then
         git clone   https://${username}:${Private_key}@github.com/pruebasunillanosdaniel/react_interface_proyectotesis
@@ -45,16 +52,27 @@ else
         cd ..
 fi
 
+echo -e  "4 $PWD"
 if [ ! -d "pdf_server_spring" ] ;then
         git clone   https://${username}:${Private_key}@github.com/pruebasunillanosdaniel/pdf_server_spring
 else
-        cd pdf_server_spring
+         cd  pdf_server_spring
         git pull   https://${username}:${Private_key}@github.com/pruebasunillanosdaniel/pdf_server_spring main
         cd ..
 fi
+echo -e  "5 $PWD"
+if [ ! -d "certif" ] ;then
+        echo  -e "creando certificado"
+        mkdir -p certif
+        cd certif/
+        sudo openssl req -newkey rsa:2048 -noenc -keyout key.key -out cert.csr
+        cd ..
+fi
 
-cd ../
+cd ..
+
 pwd 
+sudo docker compose kill
 sudo docker compose down -v 
 #sudo docker volume rm -a
-sudo docker compose -f docker-compose.yml  up --build
+sudo docker compose -f ./docker-compose.yml  up --build  --force-recreate --remove-orphans
